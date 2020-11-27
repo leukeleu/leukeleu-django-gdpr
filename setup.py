f78d3f41e4c82d00a66c0b25842d7f71696ea8eb
Wouter de Vries <wouter@oxyzen.nl>
2020-11-27 14:52:46 +0100

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-gdpr",
-    version="0.4.0",
+    version="0.4.1",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
