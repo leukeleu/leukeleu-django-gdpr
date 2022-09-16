5ea9dbde285c821ceb9f9b8d297e8173a928df4b
Wouter de Vries <wdevries@leukeleu.nl>
2022-09-16 13:09:41 +0200

    Rename package to leukeleu-django-gdpr and introduce pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="django-gdpr",
-    version="0.4.1",
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    zip_safe=False,
-    url="https://bitbucket.org/leukeleu/django-gdpr/",
-    author="Wouter de Vries (Leukeleu)",
-    author_email="wdevries@leukeleu.nl",
-    maintainer="Leukeleu",
-    maintainer_email="info@leukeleu.nl",
-    description="Django GDPR",
-    long_description="Django Gee-dee-pee-arr",
-    keywords=["gdpr", "django"],
-    classifiers=[
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "License :: Other/Proprietary License",
-    ],
-    license="Proprietary",
-    install_requires=["pyyaml", "requests"],
-)
+setup()
