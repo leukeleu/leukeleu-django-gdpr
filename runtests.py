2d549d63aa98369fbaf1c90033455c479eaaa10a
Fabian van der Post <fvanderpost@leukeleu.nl>
2025-07-28 11:28:12 +0200

    Update github actions setup

--- runtests.py
+++ runtests.py
@@ -4,11 +4,16 @@ import sys
 
 
 def runtests():
-    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
-    from django.core.management import execute_from_command_line
-
-    argv = [sys.argv[0], "test", *sys.argv[1:]]
-    execute_from_command_line(argv)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.test_settings")
+    try:
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line([sys.argv[0], "test", *sys.argv[1:]])
 
 
 if __name__ == "__main__":
